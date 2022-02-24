45af7c0504339e2855dd8f2881da57a13dbcb785
pashakiz <pashakiz@gmail.com>
2022-02-24 18:47:44 +0300

    refactor: add react-tree-vis

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import NodeCircle from './NodeCircle';
-import { Stage, Layer, Text } from 'react-konva';
+import React from 'react'
+import { BinarySearchTree, useBinarySearchTree } from 'react-tree-vis'
+import { useState } from 'react'
 
-export default class App extends React.Component {
+export default function App() {
 
-  render() {
-    return (
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          <Text text="Try click on rect" />
-          <NodeCircle />
-        </Layer>
-      </Stage>
-    );
-  }
+  const { ref, insert, remove } = useBinarySearchTree()
+
+  const [insertValue, setInsertValue] = useState(0)
+  const [removeValue, setRemoveValue] = useState(0)
+
+  return (
+    <div>
+      <input
+        type="number"
+        onChange={(elem) => setInsertValue(parseInt(elem.currentTarget.value))}
+      />
+      <button onClick={() => insert(insertValue)}>Insert</button>
+      <br />
+      <input
+        type="number"
+        onChange={(elem) => setRemoveValue(parseInt(elem.currentTarget.value))}
+      />
+      <button onClick={() => remove(removeValue)}>Remove</button>
+
+      <BinarySearchTree data={[2, 1, 3]} ref={ref} />
+    </div>
+  );
 
 }
--- src/components/Node.js
+++
@@ -1,9 +0,0 @@
-export default class Node {
-  constructor(data) {
-    this.data = data;
-    this.left = null;
-    this.right = null;
-    // this.x = x;
-    // this.y = y;
-  }
-}
